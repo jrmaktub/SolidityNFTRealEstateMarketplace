43f1d17486c86ce89c057c19029a78ce83524ae4
jramirez09 <jramirez12@luc.edu>
2024-07-18 12:03:24 -0600

    finished first tests

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -14,7 +14,19 @@ contract RealEstate is ERC721URIStorage{
     }
 
     function mint(string memory tokenURI) public returns(uint256) {
+        _tokenIds.increment();
 
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+
+
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
     }
 
 }
