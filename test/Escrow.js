3ff9fdebf18c59377a205a183b63ba75bfaba6df
jramirez09 <jramirez12@luc.edu>
2024-07-19 14:27:05 -0600

    listing and testing added

--- test/Escrow.js
+++ test/Escrow.js
@@ -31,6 +31,14 @@ describe('Escrow', () => {
             lender.address
         )
 
+        //Approve property
+        transaction = await realEstate.connect(seller).approve(escrow.address, 1)
+        await transaction.wait()
+
+        //list property
+        transaction = await escrow.connect(seller).list(1, buyer.address, tokens(10), tokens(5))
+        await transaction.wait()
+
     })
 
     describe('Deployment', () => {
@@ -58,6 +66,38 @@ describe('Escrow', () => {
             const result = await escrow.lender()
             expect(result).to.be.equal(lender.address)
         })
+
+
+    })
+
+    describe('Listing', () => {
+
+        it('Updates as Listed', async () => {
+            const result = await escrow.isListed(1);
+            expect(result).to.be.equal(true)
+        })
+
+        it('Updates Ownership', async () => {
+            expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
+
+        })
+
+        it('Returns Buyer', async () => {
+            const result = await escrow.buyer(1);
+            expect(result).to.be.equal(buyer.address)
+        })
+
+
+        it('Returns Purchase Price', async () => {
+            const result = await escrow.purchasePrice(1);
+            expect(result).to.be.equal(tokens(10))
+        })
+
+        it('Returns Escrow Amount', async () => {
+            const result = await escrow.escrowAmount(1);
+            expect(result).to.be.equal(tokens(5))
+        })
+
 
 
     })
